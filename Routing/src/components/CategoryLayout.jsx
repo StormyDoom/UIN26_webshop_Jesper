0e9487bebb810ee2d241eddfed1e0713cc555c7c
StormyDoom <jesper.simensen19@gmail.com>
2026-02-20 13:56:39 +0100

    Cha chinga
    Chopped

--- Routing/src/components/CategoryLayout.jsx
+++ Routing/src/components/CategoryLayout.jsx
@@ -6,14 +6,17 @@ export default function CategoryLayout(){
     const [apiData, setApiData] = useState([])
     const [apiEndpoint, setApiEndpoint] = useState()
 
+    const defaultApiUrl = 'https://pokeapi.co/api/v2/'
+
     const getData = async()=>{
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10')
+        const response = await fetch(defaultApiUrl)
         const data = await response.json()
-        setApiData(data.results) 
+        const {type, pokemon, item} = data
+        setApiData({type, pokemon, item}) 
   }
 
-   console.log(apiData)
-   console.log(apiEndpoint)
+   console.log("Sjekk", apiData)
+   console.log("Denne kommer fra Layout", apiEndpoint)
 
   useEffect(()=>{
     getData()
@@ -22,10 +25,11 @@ export default function CategoryLayout(){
     return (
     <>
         <nav className="main-nav">
+            {Object.keys(apiData)?.map((item) => <Link key={item+'saj'} to={item} onClick={()=> setApiEndpoint(defaultApiUrl + item)}>{item}</Link>)}
             {/* Spørsmåltegn betyr "Hvis det finnes" */}
-            {apiData?.map((item) => <Link key={item.name +'-xt'} to={item.name} onClick={()=> setApiEndpoint(item.url)}>{item.name}</Link>)}
+            {/*apiData?.map((item) => <Link key={item.name +'-xt'} to={item.name} onClick={()=> setApiEndpoint(item.url)}>{item.name}</Link>)*/}
         </nav>
-        <Outlet />
+        <Outlet context={{apiEndpoint, defaultApiUrl}} />
     </>
     )
 }
